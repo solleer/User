b82c4a728be7e3956f0bf12d16514fa0807513a7
Richard <solleer@hotmail.com>
2017-04-26 07:14:25 -0400

    Fix bugs with functions in CurrentUser

--- src/Model/CurrentUser.php
+++ src/Model/CurrentUser.php
@@ -6,16 +6,16 @@ class CurrentUser {
     private $user;
     private $status;
 
-    public function __construct(\User\Model\User $user, SigninStatus $status) {
+    public function __construct(User $user, SigninStatus $status) {
         $this->user = $user;
         $this->status = $status;
     }
 
     public function updateCurrentUser(array $data) {
-        return $this->save($data, $this->status->getSigninID());
+        return $this->user->save($data, $this->status->getSigninID());
     }
 
     public function getCurrentUser() {
-        return $this->getUser($this->status->getSigninID());
+        return $this->user->getUser($this->status->getSigninID());
     }
 }
